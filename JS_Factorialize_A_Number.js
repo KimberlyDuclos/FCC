51cf24585356d96244a4eeea1c4e79d1bd0dc9c7
Kim Duclos <kimduclos@gmail.com>
2018-05-10 10:25:13 -0600

    Update Factorial

--- JS_Factorialize_A_Number.js
+++ JS_Factorialize_A_Number.js
@@ -1,15 +1,15 @@
 function factorialize(num) {  
   
   var newNum = 1;  // set var to initial number
-  var n =  num;    // set var to user input number
+  var n;    // set var to user input number
   
-  if (num <= 1){  // if statement setting !0 to 1
+  if (num <= 1){  // if statement setting 0 to 1
     console.log(1);
     num = 1;
     return num;
   }
   
-  for (var x=2; x<=num; x++){  // for loop to !num input, going no bigger than input num
+  for (var x=2; x<=num; x++){  // for loop to num input, going no bigger than input num
     n = newNum * x;  // * smallest number by next number
     newNum = n;  // reassign var to hold new product created in line above
   }
